973f02af3856378c84586b78ae4f7420b37e378c
linyoumu <linyoumu@zc-campus.cn>
2022-10-08 14:54:42 +0800

    objc4-841源码编译

---
+++ ObjcTest/main.m
@@ -0,0 +1,24 @@
+//
+//  main.m
+//  ObjcTest
+//
+//  Created by linApple on 2022/10/8.
+//
+
+#import <Foundation/Foundation.h>
+
+/*
+ ⚠️编译调试错误检查：
+ 1: enable hardened runtime -> NO
+ 2: build phase -> denpendenice -> objc
+ 3: 尝试重新创建自己的Target
+ */
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSObject *object = [NSObject alloc];
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
